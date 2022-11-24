90c267fd22cd0f89f0ebe6e708df6b7adb606de6
Nitin Sharma <nitinsharma@Nitins-MacBook.local>
2022-11-24 09:44:38 +0530

    TEST

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0;
-    [SerializeField] float yValue = 0.01f;
-    float zValue = 0;
+    [SerializeField] float moveSpeed = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +15,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
     }
 }
